e178b6cda1b5247370dd6109c241fce5aae1e5b1
MathBet-fc <filippobet4@gmail.com>
2026-03-16 17:56:08 +0100

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import engine
 # CONFIGURAZIONE API & PAGINA
 # ==============================================================================
 # 🔴 INSERISCI LA TUA CHIAVE API QUI SOTTO
-ODDS_API_KEY = "INSERISCI_QUI_LA_TUA_CHIAVE"
+ODDS_API_KEY = "1b31f14ebbc80b505c8412a5dc4d6791"
 
 st.set_page_config(page_title="Mathbet fc - ML Ultimate Pro", page_icon="🧠", layout="wide")
 
